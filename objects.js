dde6749bb90d4ab43e196f606f9d9544aa55ca32
NIRAJ  UDATA <158997622+nirajudata03@users.noreply.github.com>
2024-12-09 15:57:38 +0000

    objects with array

--- objects.js
+++ objects.js
@@ -34,6 +34,70 @@ jsUser.anotherNewObject = function(){
     
 }
 
-console.log(jsUser.newObject());
-console.log(jsUser.anotherNewObject());
+// console.log(jsUser.newObject());
+// console.log(jsUser.anotherNewObject());
 
+// ++++++++++++++Another Example & Topic+++++++++++++
+
+const tinderUser = {
+    id : "123abc",
+    name: "niraj",
+    isLoggedIn: true
+}
+
+// console.log(tinderUser);
+
+const newobject = {
+    email: "some@gmail.com",
+    fullName: {
+        userFullName:{
+            fristName: "niraj",
+            lastName: "udata"
+        }
+    }
+}
+
+// console.log(newobject.fullName.userFullName.fristName);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "e", 6: "f"}
+
+// const obj4 = {obj1, obj2, obj3}
+
+// console.log(obj4);
+
+// const obj4 = Object.assign({}, obj1, obj2, obj3)
+
+// console.log(obj4);
+
+const obj4 = {...obj1, ...obj2, ...obj3}
+
+// console.log(obj4);
+
+
+// ++++++++++Objects in Array++++++++++
+
+const arrayObject = [
+    {
+        id: "001"
+    },
+    {
+        id: "002"
+    },
+    {
+        id: "003"
+    },
+    {
+        id: "004"
+    }
+]
+
+// console.log(arrayObject);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
