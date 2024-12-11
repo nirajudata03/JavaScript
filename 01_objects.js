9b23deefe537f00dd45dfa1a1e33b87fa14cff1e
NIRAJ  UDATA <158997622+nirajudata03@users.noreply.github.com>
2024-12-11 07:27:44 +0000

    file name updated

---
+++ 01_objects.js
@@ -0,0 +1,126 @@
+// singleton
+// object.create
+
+
+// object literals
+
+const mysym = Symbol("key1")
+
+const jsUser = {
+    name : "Niraj",
+    email : "niraj@google.com",
+    age : 20,
+    loggedIn : false,
+    activeUser : true,
+    lastLoggedInDate : ["Mon", "Wed"],
+    "fullName": "Niraj Ambadas Udata",
+    mysym: "key2"
+}
+
+// console.log(jsUser.name);
+// console.log(jsUser["age"]);
+// console.log(jsUser);
+// console.log(typeof mysym);
+// Object.freeze(jsUser)
+
+
+jsUser.newObject = function(){
+    console.log(`Hello Js user:`);
+    
+}
+
+jsUser.anotherNewObject = function(){
+    console.log(`Hello JS user: ${this.name}`);
+    
+}
+
+// console.log(jsUser.newObject());
+// console.log(jsUser.anotherNewObject());
+
+// ++++++++++++++Another Example & Topic+++++++++++++
+
+const tinderUser = {
+    id : "123abc",
+    name: "niraj",
+    isLoggedIn: true
+}
+
+// console.log(tinderUser);
+
+const newobject = {
+    email: "some@gmail.com",
+    fullName: {
+        userFullName:{
+            fristName: "niraj",
+            lastName: "udata"
+        }
+    }
+}
+
+// console.log(newobject.fullName.userFullName.fristName);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "e", 6: "f"}
+
+// const obj4 = {obj1, obj2, obj3}
+
+// console.log(obj4);
+
+// const obj4 = Object.assign({}, obj1, obj2, obj3)
+
+// console.log(obj4);
+
+const obj4 = {...obj1, ...obj2, ...obj3}
+
+// console.log(obj4);
+
+
+// ++++++++++Objects in Array++++++++++
+
+const arrayObject = [
+    {
+        id: "001"
+    },
+    {
+        id: "002"
+    },
+    {
+        id: "003"
+    },
+    {
+        id: "004"
+    }
+]
+
+// console.log(arrayObject);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
+
+
+const jasonCourse = {
+    courseName: "jason",
+    coursePrise: "free",
+    courseInstructor: "niraj"
+}
+
+const {courseInstructor: instructor} = jasonCourse
+const {coursePrise: prise} = jasonCourse
+
+// console.log(courseInstructor);
+console.log(instructor);
+console.log(prise);
+
+// +++++ jason +++++
+
+// {
+//     "name": "niraj",
+//     "age": "20"
+// }
+
+
---
+++ 02_function.js
@@ -0,0 +1,75 @@
+// Functions in JS
+
+function firstFunction(){
+    // console.log("niraj");
+    
+}
+firstFunction()
+
+function loginCheck(userLoggedIn, userName){
+    if(userLoggedIn === false){
+        console.log(`${userName} Please login first`);
+        return
+    }
+    return `${userName} logged in just now`
+}
+// console.log(loginCheck(false, "Krishna"))
+
+
+function twoNumberAddition(num1, num2){
+    let total = num1 + num2
+    return total
+}
+const total = twoNumberAddition(15, 30)
+// console.log(total);
+
+
+const objectAndFunction = {
+    name: "Rohit",
+    userId: "rohit45",
+    age: 37,
+    carrier: "Cricketer"
+}
+
+function functionWithObject(anyObject){
+    return anyObject.userId
+}
+// console.log(functionWithObject(objectAndFunction));
+
+
+const functionAndArray = [100, 200, 300, 400]
+
+function functionWithArra(anyArray){
+    return anyArray[2]
+}
+// console.log(functionWithArra(functionAndArray));
+
+function calculateCartPrise(var1 , var2, ...val1){
+    return val1
+}
+// console.log(calculateCartPrise(45, 65, 25, 95));
+
+const names = {
+    firstName: "Hanuman",
+    secondName: "Ram"
+}
+
+function nameOfHero(anyName){
+    // console.log(`${anyName.firstName} and ${anyName.secondName} are best friends forever...`);
+    
+}
+nameOfHero(names)
+
+
+// ++++++++++ Some Examples ++++++++++
+
+const animalName = ["Elepant", "Tiger", "Lion", "Cheeta", "Dear", "Bear", "Fox",  "Snek",]
+
+function animalNameCheck(animal){
+    if(animal[2] === "Lion"){
+        console.log(`I fond it`);
+        return
+    }
+    return animal
+}
+animalNameCheck(animalName)
