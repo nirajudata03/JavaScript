aa3fd82e24e6006281d2157982c5bd90b714a33e
NIRAJ  UDATA <158997622+nirajudata03@users.noreply.github.com>
2024-12-12 11:14:54 +0000

    learn array functon

--- 04_arrowFunction.js
+++ 04_arrowFunction.js
@@ -1,8 +1,41 @@
 // ++++++++++ Arrow Function ++++++++++
 
-const arrowFunction = (num1, num2) => {
-   let numTotal = num1 - num2
-   return numTotal 
+
+const chai = () => {
+   console.log(`This is Arrow Function`);
+   
 }
-const numTotal = arrowFunction(10,5)
-console.log(numTotal);
+// chai() // arrow funtion defining method
+
+
+
+// const arrowFunction = (num1, num2) => {
+//    let numTotal = num1 - num2
+//    return numTotal 
+// }
+// const numTotal = arrowFunction(10,5)
+// console.log(numTotal);
+
+
+function one(){
+   let name = "niraj"
+   console.log(this);
+}
+// one()
+
+
+
+// const addtwo = (num1, num2) => {
+//    return num1 + num2
+// }
+
+// const addtwo = (num1, num2) =>  num1 + num2
+
+// const addtwo = (num1, num2) =>  (num1 + num2)
+
+const addtwo = (num1, num2) =>  { return num1 + num2}
+
+// const addtwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addtwo(40,5));
