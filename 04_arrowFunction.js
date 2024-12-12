c56d47acca6995d1e3bace49f50c1127132b6b92
NIRAJ  UDATA <158997622+nirajudata03@users.noreply.github.com>
2024-12-12 10:38:44 +0000

    arrowfunction file created

---
+++ 04_arrowFunction.js
@@ -0,0 +1,8 @@
+// ++++++++++ Arrow Function ++++++++++
+
+const arrowFunction = (num1, num2) => {
+   let numTotal = num1 - num2
+   return numTotal 
+}
+const numTotal = arrowFunction(10,5)
+console.log(numTotal);
