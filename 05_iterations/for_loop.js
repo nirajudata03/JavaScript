a49c1dc56c3264a939e5e0df96dea1031a25e9a1
NIRAJ  UDATA <158997622+nirajudata03@users.noreply.github.com>
2024-12-13 10:32:03 +0000

    for loop file created

---
+++ 05_iterations/for_loop.js
@@ -0,0 +1,33 @@
+// For Loop in JS
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    // console.log(i);
+}
+
+for (let i = 1; i < 10; i++) {
+    // console.log(`Table of ${i}`);
+    
+    for (let j = 1; j < 10; j++) {
+        // console.log(`${i} * ${j} = ${i * j}`);
+        
+    }
+    
+}
+
+// const myArray = ["apple", "ball", "cat", "dog", "elepent", "fish", "god", "high", "image", "jack", "king", "lion", "moon", "niraj"]
+// const alpa = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"]
+
+// for (let i = 0; i < myArray.length; i++) {
+    
+//     for (let j = 0; j < alpa.length; j++) {
+//         console.log(`${alpa[i]} for ${myArray[i]}`);
+//         if () {
+//             break
+//         }
+        
+//     }
+// }
+
+
+
